cf5bdff75a3420d2efde1ba86f7ca2946198aa1b
Avi Oza <avioza@gmail.com>
2019-10-14 11:16:57 -0400

    add setup py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name='pi-funcs',
+        version='0.1',
+        packages=['pi_funcs',],
+        license='TBD',
+        long_description='TBD'
+        )
