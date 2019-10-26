a7fb6f14ca0d5121d44626ed2b9cef420510f87f
Avi Oza <avioza@gmail.com>
2019-10-26 10:03:20 -0400

    fix bug for bytes

--- pi_funcs/funcs.py
+++ pi_funcs/funcs.py
@@ -4,7 +4,7 @@ import subprocess
 def get_ds18b20_sensor(sensor_id):
     command_to_run="""cat /sys/bus/w1/devices/{0}/w1_slave""".format(sensor_id)
     output = subprocess.check_output([command_to_run], shell=True)
-    raw_value = output.rsplit('=', 1)[1].strip()
+    raw_value = output.rsplit(b'=', 1)[1].strip()
 
     # Convert raw_value to F
     f_degrees = 1.8*float(raw_value)/1000 + 32
